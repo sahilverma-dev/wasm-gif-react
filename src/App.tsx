88f6806b2d5bcc28d2e780c64669625930380f08
Sahil Verma <domainsahil007@gmail.com>
2026-01-30 11:31:51 +0530

    feat: Initialize React project with Vite, TypeScript, and ESLint configuration.

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile, toBlobURL } from "@ffmpeg/util";
+
+import { useRef, useState } from "react";
+
+const App = () => {
+  const [loaded, setLoaded] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
+
+  const load = async () => {
+    const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
+    const ffmpeg = ffmpegRef.current;
+    ffmpeg.on("log", ({ message }) => {
+      if (messageRef.current) messageRef.current.innerHTML = message;
+      console.log(message);
+    });
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        "application/wasm",
+      ),
+    });
+    setLoaded(true);
+  };
+
+  const transcode = async () => {
+    const ffmpeg = ffmpegRef.current;
+    await ffmpeg.writeFile(
+      "input.webm",
+      await fetchFile(
+        "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/Big_Buck_Bunny_180_10s.webm",
+      ),
+    );
+    await ffmpeg.exec(["-i", "input.webm", "output.mp4"]);
+    const data = await ffmpeg.readFile("output.mp4");
+    if (videoRef.current) {
+      videoRef.current.src = URL.createObjectURL(
+        new Blob([(data as Uint8Array).buffer as ArrayBuffer], {
+          type: "video/mp4",
+        }),
+      );
+    }
+  };
+
+  return loaded ? (
+    <>
+      <video ref={videoRef} controls></video>
+      <br />
+      <button onClick={transcode}>Transcode webm to mp4</button>
+      <p ref={messageRef}></p>
+      <p>Open Developer Tools (Ctrl+Shift+I) to View Logs</p>
+    </>
+  ) : (
+    <button onClick={load}>Load ffmpeg-core (~31 MB)</button>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
+});
